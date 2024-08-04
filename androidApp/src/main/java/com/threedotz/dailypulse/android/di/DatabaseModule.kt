756cfb5081b52c34ab97d6b58a9be31d1f915bf7
Syarif <syarif.nur92@gmail.com>
2024-08-04 22:57:43 +0700

    update fix database

--- androidApp/src/main/java/com/threedotz/dailypulse/android/di/DatabaseModule.kt
+++ androidApp/src/main/java/com/threedotz/dailypulse/android/di/DatabaseModule.kt
@@ -7,7 +7,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module{
-    single<SqlDriver>{DatabaseDriverFactory(androidContext()).createDriverFunction()}
+    single<SqlDriver>{DatabaseDriverFactory(androidContext()).createDriver()}
 
     single<DailyPulseDatabase>{DailyPulseDatabase(get())}
 }
--- shared/src/androidMain/kotlin/com/threedotz/dailypulse/db/DatabaseDriverFactory.kt
+++ shared/src/androidMain/kotlin/com/threedotz/dailypulse/db/DatabaseDriverFactory.kt
@@ -5,11 +5,12 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import come.threedotz.dailypulse.db.DailyPulseDatabase
 
-actual class DatabaseDriverFactory(private val context: Context) {
+actual  class DatabaseDriverFactory(private val context: Context) {
 
-    actual fun createDriverFunction(): SqlDriver = AndroidSqliteDriver(
+    actual fun createDriver(): SqlDriver = AndroidSqliteDriver(
         schema = DailyPulseDatabase.Schema,
         context = context,
         name = "DailyPulseDatabase.db"
     )
+
 }
