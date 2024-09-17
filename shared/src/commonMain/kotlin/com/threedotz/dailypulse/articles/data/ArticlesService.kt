5d153022dee0d67ddf7220564b3d95e61f3220e5
Syarif <syarif.nur92@gmail.com>
2024-09-17 20:58:04 +0700

    Update ArticlesService.kt

--- shared/src/commonMain/kotlin/com/threedotz/dailypulse/articles/data/ArticlesService.kt
+++ shared/src/commonMain/kotlin/com/threedotz/dailypulse/articles/data/ArticlesService.kt
@@ -6,11 +6,11 @@ import io.ktor.client.request.get
 
 class ArticlesService(private val httpClient: HttpClient) {
     private val country = "us"
-    private val category = "business"
+    private val category = "general"
     private val apiKey = "2024bd0744f44f62808393e09ef66ca8"
 
     suspend fun fetchArticles(): List<ArticleRaw>{
-        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?country=${country}&category=$category&apiKey=$apiKey").body()
+        val response: ArticlesResponse = httpClient.get("https://newsapi.org/v2/top-headlines?country=$country&apiKey=$apiKey&category=$category").body()
         return response.articles
     }
 }
